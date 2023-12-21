828ae8753313044f21053290e6b2d0d9fa26027c
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-12-21 15:00:28 +0530

    api added

--- src/components/CurrencyConvertorBody.js
+++ src/components/CurrencyConvertorBody.js
@@ -1,13 +1,58 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const CurrencyConvertorBody = () => {
+  const [exchangeRate, setExchangeRate] = useState({});
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://v6.exchangerate-api.com/v6/9345b9b99681eeed742d82e9/latest/${fromCurrency}`;
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        setExchangeRate(res.data.conversion_rates);
+      })
+      .catch((error) => {
+        console.error("Error fetching exchange rates:", error);
+      });
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    const conersionRate = exchangeRate[toCurrency];
+    if (conersionRate) {
+      const Converted = amount * conersionRate;
+      setConvertedAmount(Converted.toFixed(2));
+    }
+  }, [amount, fromCurrency, toCurrency, exchangeRate]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "amount":
+        setAmount(value);
+        console.log(amount);
+        break;
+      case "fromCurrency":
+        setFromCurrency(value);
+        break;
+      case "toCurrency":
+        setToCurrency(value);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <div className="card">
         <div className="titlediv">
           <img src="/images/rupee.png" alt="rupee" className="rupee" />
-
-          <h1 className="titleName">Currency Converter</h1>
+          <div className="titleName">
+            <h1>Currency Converter</h1>
+          </div>
         </div>
 
         <div className="currency_exchange">
@@ -15,19 +60,32 @@ const CurrencyConvertorBody = () => {
             <label className="input_lable">
               <b> Amount:</b>
             </label>
-            <input type="number" name="amount" className="input_field" />
+            <input
+              type="text"
+              name="amount"
+              value={amount}
+              onChange={handleChange}
+              className="input_field"
+            />
           </div>
 
           <div className="input_container">
             <label className="input_lable">
               <b>From Currency:</b>
             </label>
-            <select name="currency" className="input_field">
-              <option value="sangram">sangram</option>
-              <option value="rutika">Rutika</option>
-              <option value="Shobha">Shobha</option>
-              <option value="Dadaso">Dadaso</option>
-              <option value="Varsha">Varsha</option>
+            <select
+              name="fromCurrency"
+              value={fromCurrency}
+              onChange={handleChange}
+              className="input_field"
+            >
+              {Object.keys(exchangeRate).map((currency) => {
+                return (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                );
+              })}
             </select>
           </div>
 
@@ -35,18 +93,27 @@ const CurrencyConvertorBody = () => {
             <label className="input_lable">
               <b>To Currency:</b>
             </label>
-            <select name="currency" className="input_field">
-              <option value="sangram">sangram</option>
-              <option value="rutika">Rutika</option>
-              <option value="Shobha">Shobha</option>
-              <option value="Dadaso">Dadaso</option>
-              <option value="Varsha">Varsha</option>
+            <select
+              name="toCurrency"
+              value={toCurrency}
+              onChange={handleChange}
+              className="input_field"
+            >
+              {Object.keys(exchangeRate).map((currency) => {
+                return (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                );
+              })}
             </select>
           </div>
         </div>
 
         <div className="output">
-          <h2 className="output_amount">Converted Amount:6969</h2>
+          <h2>
+            Converted Amount : {convertedAmount} {toCurrency}
+          </h2>
         </div>
       </div>
     </>
